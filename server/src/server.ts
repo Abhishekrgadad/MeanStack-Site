10c333eeeaeb196b3abf3644dfee13459c8e144e
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-25 23:26:56 +0530

    added all crud operations

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { connectToDatabase } from "./database";
+import { employeeRouter } from "./employee.routes";
 
 dotenv.config();
 
@@ -16,10 +17,12 @@ connectToDatabase(ATLAS_URI)
     .then(() => {
         const app = express();
         app.use(cors());
+
+        app.use('/employees', employeeRouter);
         
         app.listen(5200, ()=>{
             console.log(`Server is running at http://localhost:5200`);
         });
     })
     .catch((error) => console.error(error));
-    
+
